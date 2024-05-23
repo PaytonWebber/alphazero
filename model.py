55aa35a0dac5266f9647b8b46bfacdc8682ec342
Payton Webber <paytonwebber@uvic.ca>
2024-05-23 23:41:04 +0000

    Added MLP layer after ResNet

--- model.py
+++ model.py
@@ -4,13 +4,28 @@ import torch.nn.functional as F
 from torch import optim as optimizer
 import numpy as np
 
+class MLP(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers=1):
+
+        super(MLP, self).__init__()
+        self.fc_in = nn.Linear(input_dim, hidden_dim)
+        self.fc_hidden = nn.ModuleList([nn.Linear(hidden_dim, hidden_dim) for _ in range(num_layers-1)])
+        self.fc_out = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc_in(x))
+        for layer in self.fc_hidden:
+            x = F.relu(layer(x))
+        x = self.fc_out(x)
+        return x
+
 
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(ResidualBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
 
     def forward(self, x):
@@ -21,8 +36,9 @@ class ResidualBlock(nn.Module):
         out = F.relu(out)
         return out
 
+
 class AlphaZeroResNet(nn.Module):
-    def __init__(self, num_residual_blocks, num_channels=64):
+    def __init__(self, num_residual_blocks, num_hidden_layers, num_channels=64):
         super(AlphaZeroResNet, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=num_channels//2, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(num_channels//2)
@@ -32,35 +48,31 @@ class AlphaZeroResNet(nn.Module):
         self.residual_blocks = nn.Sequential(
             *[ResidualBlock(num_channels, num_channels) for _ in range(num_residual_blocks)]
         )
-        
+
         # Policy head
-        self.policy_conv = nn.Conv2d(in_channels=num_channels, out_channels=2, kernel_size=1)
-        self.policy_bn = nn.BatchNorm2d(2)
-        self.policy_fc = nn.Linear(2 * 3 * 3, 9)  # 9 possible moves in tic-tac-toe
-        
+        self.mlp_policy = MLP(num_channels*3*3, 256, 9, num_hidden_layers)
+        self.softmax = nn.Softmax(dim=1)
+
         # Value head
-        self.value_conv = nn.Conv2d(in_channels=num_channels, out_channels=1, kernel_size=1)
-        self.value_bn = nn.BatchNorm2d(1)
-        self.value_fc1 = nn.Linear(1 * 3 * 3, 64)
-        self.value_fc2 = nn.Linear(64, 1)
+        self.mlp_value = MLP(num_channels*3*3, 256, 1, num_hidden_layers)
+        self.tanh = nn.Tanh()
         
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = self.residual_blocks(x)
-        
+        x = x.view(-1, 64*3*3)
+
         # Policy head
-        policy = F.relu(self.policy_bn(self.policy_conv(x)))
-        policy = policy.view(-1, 2 * 3 * 3)
-        policy = F.softmax(self.policy_fc(policy), dim=1)
-        
+        pi = self.mlp_policy(x)
+        pi = self.softmax(pi)
+
         # Value head
-        value = F.relu(self.value_bn(self.value_conv(x)))
-        value = value.view(-1, 1 * 3 * 3)
-        value = F.relu(self.value_fc1(value))
-        value = torch.tanh(self.value_fc2(value))
-        
-        return policy, value
+        v = self.mlp_value(x)
+        v = self.tanh(v)
+
+        return pi, v
+
 
 class AlphaZeroModel:
 
@@ -68,6 +80,7 @@ class AlphaZeroModel:
         self.config = config
         self.model = AlphaZeroResNet(
             config.num_blocks,
+            config.num_hidden_layers
         )
 
         if not config.from_scratch:
